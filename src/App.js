867cdb25863ee6c756fe92bc4fab1b8cd2837017
Joshua Burger <joshberger6969@outlook.com>
2024-07-08 19:38:51 -0400

    Import grill page, Add route path for grill component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
 import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Grill from "./pages/Grill";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="grill" element={<Grill />} />
         </Route>
       </Routes>
     </BrowserRouter>

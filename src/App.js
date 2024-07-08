c51348c411e280887dcc4cfd3be844e24a68b125
Joshua Burger <joshberger6969@outlook.com>
2024-07-08 19:42:36 -0400

    Import Cafe component, Add route for cafe

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import Grill from "./pages/Grill";
+import Cafe from "./pages/Cafe";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="grill" element={<Grill />} />
+          <Route path="cafe" element={<Cafe />} />
         </Route>
       </Routes>
     </BrowserRouter>

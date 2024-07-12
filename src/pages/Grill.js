b8d18d22a84cc2e4db043160c178c357028a2e99
Joshua Burger <joshberger6969@outlook.com>
2024-07-12 17:20:43 -0400

    Add menu and CSS

--- src/pages/Grill.js
+++ src/pages/Grill.js
@@ -1,50 +1,107 @@
+import "../grill.css";
+
+
 const Grill = () => {
     return <>
-      <h1>Boomers Grill Menu</h1>;
+      <div class="menu">
+      <h1 class="color_change">Boomers Grill Menu</h1>;
       <br></br>
-      <h2>Starters</h2>
-      <h4>Chicken Wings</h4>
-      <h5>12 Jumbo Wings, choice of Sweet, Hot, Mild, Teriyaki, Garlic Parm</h5>
+      <h2 class="color_change">Starters</h2>
       <br></br>
-      <h4>Pickle Fry</h4>
+      <h4 class="h4_change">Chicken Wings</h4>
+      <h5 class="h5_change">12 Jumbo Wings, choice of Sweet, Hot, Mild, Teriyaki, Garlic Parm</h5>
       <br></br>
-      <h4>3 Spring Roll's</h4>
+      <h4 class="h4_change">Pickle Fry</h4>
       <br></br>
-      <h4>3 Philly Cheese Egg Roll's</h4>
+      <h4 class="h4_change">3 Spring Roll's</h4>
       <br></br>
-      <h4>Flat Bread Pizza</h4>
-      <h5>Pick 2: Chicken, Sausage, Peppers, Onions, Assorted Veggies, Philly Meat, Mozzarella Cheese</h5>
+      <h4 class="h4_change">3 Philly Cheese Egg Roll's</h4>
       <br></br>
-      <h4>Burnt Ends with Bourbon Sauce</h4>
+      <h4 class="h4_change">Flat Bread Pizza</h4>
+      <h5 class="h5_change">Pick 2: Chicken, Sausage, Peppers, Onions, Assorted Veggies, Philly Meat, Mozzarella Cheese</h5>
       <br></br>
-      <h4>Deep Fried Brussel Sprouts</h4>
-      <h5>With Ranch Dressing</h5>
+      <h4 class="h4_change">Burnt Ends with Bourbon Sauce</h4>
       <br></br>
-      <h4>Jalapeno Poppers</h4>
+      <h4 class="h4_change">Deep Fried Brussel Sprouts</h4>
+      <h5 class="h5_change">With Ranch Dressing</h5>
       <br></br>
-      <h4>Chicken Tenders</h4>
-      <h5>Choice of Sauce</h5>
+      <h4 class="h4_change">Jalapeno Poppers</h4>
       <br></br>
-      <h4>Large Soft Pretzel</h4>
-      <h5>With Honey Mustard and Beer Cheese</h5>
+      <h4 class="h4_change">Chicken Tenders</h4>
+      <h5 class="h5_change">Choice of Sauce</h5>
       <br></br>
-      <h2>Main's</h2>
+      <h4 class="h4_change">Large Soft Pretzel</h4>
+      <h5 class="h5_change">With Honey Mustard and Beer Cheese</h5>
       <br></br>
-      <h3>Sandwiches</h3>
+      <hr class="line_through"></hr>
       <br></br>
-      <h4>Dirty Harry Melt</h4>
-      <h5>Grilled Steak with Onions, topped with Muenster Cheese on a toasted Garlic Roll</h5>
+      <h2 class="color_change">Main's</h2>
       <br></br>
-      <h4>Bonnie & Clyde</h4>
-      <h5>Grilled Turkey & Pastrami topped with Cole Slaw and Swiss on grilled Rye Bread</h5>
+      <h3 class="color_change">Sandwiches</h3>
       <br></br>
-      <h4>Beam-Me-Up Rueben</h4>
-      <h5>Corned Beef or Pastrami, Sauerkraut, Swiss Cheese, Thousand Island Dressing, on grilled Rye Bread</h5>
+      <h4 class="h4_change">Dirty Harry Melt</h4>
+      <h5 class="h5_change">Grilled Steak with Onions, topped with Muenster Cheese on a toasted Garlic Roll</h5>
       <br></br>
-      <h4>Rooster Cogburn</h4>
-      <h5>Chicken Culet with Roasted Peppers, Mozzarella Cheese, Lettuce, Tomato, & Onion, topped with Pesto Mayonnaise on Italian Bread</h5>
+      <h4 class="h4_change">Bonnie & Clyde</h4>
+      <h5 class="h5_change">Grilled Turkey & Pastrami topped with Cole Slaw and Swiss on grilled Rye Bread</h5>
       <br></br>
-      <h4>Bewitched Turkey Gobbler</h4>
+      <h4 class="h4_change">Beam-Me-Up Rueben</h4>
+      <h5 class="h5_change">Corned Beef or Pastrami, Sauerkraut, Swiss Cheese, Thousand Island Dressing, on grilled Rye Bread</h5>
+      <br></br>
+      <h4 class="h4_change">Rooster Cogburn</h4>
+      <h5 class="h5_change">Chicken Culet with Roasted Peppers, Mozzarella Cheese, Lettuce, Tomato, & Onion, topped with Pesto Mayonnaise on Italian Bread</h5>
+      <br></br>
+      <h4 class="h4_change">Bewitched Turkey Gobbler</h4>
+      <h5 class="h5_change">8oz. Sliced Fresh Turkey with bacon, Muenster Cheese, Lettuce, Tomato, topped with Cranberry Mayonnaise on a Hard Roll</h5>
+      <br></br>
+      <h4 class="h4_change">The Fonz Fish Fry</h4>
+      <h5 class="h5_change">8oz. Haddock, with Fonzie's breading & Remoulade Sauce, on a Hoagie Roll</h5>
+      <br></br>
+      <h4 class="h4_change">Edith's Grilled Cheese Sandwich</h4>
+      <h5 class="h5_change">Choice of Cheese & Bread, with/without Ham</h5>
+      <br></br>
+      <h4 class="h4_change">Gomer's BLT</h4>
+      <h5 class="h5_change">Piled high with Bacon, Lettuce, Tomato, & Mayonnaise on choice of Bread</h5>
+      <br></br>
+      <h4 class="h4_change">The Balboa</h4>
+      <h5 class="h5_change">Hot Italian Sausage, Hot Pepper, Onions, on a Hoagie Roll</h5>
+      <br></br>
+      <hr class="line_through"></hr>
+      <br></br>
+      <h3 class="color_change">Salads</h3>
+      <br></br>
+      <h4 class="h4_change">Chef's Salad</h4>
+      <h5 class="h5_change">Greens piled high with Ham, Turkey, Swiss Cheese, Hard-boiled Egg, Tomato, & Onion</h5>
+      <br></br>
+      <h4 class="h4_change">Greek Salad</h4>
+      <h5 class="h5_change">Romaine Lettuce, Pepperoncini, Tomato, Cucumber, Black Olives & Red Onion</h5>
+      <br></br>
+      <h4 class="h4_change">Caesar Salad</h4>
+      <h5 class="h5_change">add Chicken / add Shrimp</h5>
+      <br></br>
+      <hr class="line_through"></hr>
+      <br></br>
+      <h3 class="color_change">Burgers</h3>
+      <br></br>
+      <h4 class="h4_change">The Lenny & Squiggy</h4>
+      <h5 class="h5_change">8oz. burger with Cheese, topped with Creamy Cole Slaw on a Hard Roll</h5>
+      <br></br>
+      <h4 class="h4_change">The Two Youts</h4>
+      <h5 class="h5_change">Two 6oz burgers with Lettuce, Tomato, Onion & Secret Sauce on a Hard Roll</h5>
+      <br></br>
+      <h4 class="h4_change">Meathead Patty Melt</h4>
+      <h5 class="h5_change">Hamburger Patty with Grilled Onions on Grilled Rye with Cheese</h5>
+      <br></br>
+      <h4 class="h4_change">"I Get No Respect"</h4>
+      <h5 class="h5_change">8oz Turkey Burger with Lettuce, Tomato, Red Onion & cheese topped with Cranberry Mayonnaise on a Hard Roll</h5>
+      <br></br>
+      <h4 class="h4_change">The Gangster</h4>
+      <h5 class="h5_change">8oz Fresh Ground Beef Burger, topped with Lettuce, Tomato, Red Onion, & Cheese on a Hard roll</h5>
+      <br></br>
+      <h4 class="h4_change">Fried Flintstone Burger</h4>
+      <h5 class="h5_change">16oz. burger with Sauteed Onions, Cheese, Lettuce & Tomato on a Hard Roll</h5>
+      <br></br>
+      </div>
     </>
   };
   

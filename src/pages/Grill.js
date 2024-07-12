b70ec9f97fbfe6ed3dda8c26cd80ae7c130c2c6f
Joshua Burger <joshberger6969@outlook.com>
2024-07-11 20:39:09 -0400

    Add menu to boomers grill

--- src/pages/Grill.js
+++ src/pages/Grill.js
@@ -1,5 +1,51 @@
 const Grill = () => {
-    return <h1>Grill</h1>;
+    return <>
+      <h1>Boomers Grill Menu</h1>;
+      <br></br>
+      <h2>Starters</h2>
+      <h4>Chicken Wings</h4>
+      <h5>12 Jumbo Wings, choice of Sweet, Hot, Mild, Teriyaki, Garlic Parm</h5>
+      <br></br>
+      <h4>Pickle Fry</h4>
+      <br></br>
+      <h4>3 Spring Roll's</h4>
+      <br></br>
+      <h4>3 Philly Cheese Egg Roll's</h4>
+      <br></br>
+      <h4>Flat Bread Pizza</h4>
+      <h5>Pick 2: Chicken, Sausage, Peppers, Onions, Assorted Veggies, Philly Meat, Mozzarella Cheese</h5>
+      <br></br>
+      <h4>Burnt Ends with Bourbon Sauce</h4>
+      <br></br>
+      <h4>Deep Fried Brussel Sprouts</h4>
+      <h5>With Ranch Dressing</h5>
+      <br></br>
+      <h4>Jalapeno Poppers</h4>
+      <br></br>
+      <h4>Chicken Tenders</h4>
+      <h5>Choice of Sauce</h5>
+      <br></br>
+      <h4>Large Soft Pretzel</h4>
+      <h5>With Honey Mustard and Beer Cheese</h5>
+      <br></br>
+      <h2>Main's</h2>
+      <br></br>
+      <h3>Sandwiches</h3>
+      <br></br>
+      <h4>Dirty Harry Melt</h4>
+      <h5>Grilled Steak with Onions, topped with Muenster Cheese on a toasted Garlic Roll</h5>
+      <br></br>
+      <h4>Bonnie & Clyde</h4>
+      <h5>Grilled Turkey & Pastrami topped with Cole Slaw and Swiss on grilled Rye Bread</h5>
+      <br></br>
+      <h4>Beam-Me-Up Rueben</h4>
+      <h5>Corned Beef or Pastrami, Sauerkraut, Swiss Cheese, Thousand Island Dressing, on grilled Rye Bread</h5>
+      <br></br>
+      <h4>Rooster Cogburn</h4>
+      <h5>Chicken Culet with Roasted Peppers, Mozzarella Cheese, Lettuce, Tomato, & Onion, topped with Pesto Mayonnaise on Italian Bread</h5>
+      <br></br>
+      <h4>Bewitched Turkey Gobbler</h4>
+    </>
   };
   
 export default Grill;

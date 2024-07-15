9e4407b8ec1303a9bc590616cd5f2bd33a718cb6
Joshua Burger <joshberger6969@outlook.com>
2024-07-15 17:43:25 -0400

    Remove navbar brand

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -9,7 +9,6 @@ const Layout = () => {
     <>
       <Navbar sticky="top" bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="/">Boomers Cafe & Grill</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="Grill">Grill</Nav.Link>
